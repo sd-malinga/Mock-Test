c6c2e18eab31f11d745068cff95b0b7819c52f1b
Somyaditya Deopuria <65103947+sd-malinga@users.noreply.github.com>
2022-05-24 21:01:49 +0530

    Added Comments

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -5,9 +5,28 @@ pragma solidity ^0.8.0;
 
 import './fractional.sol';
 
+
+/// @title A contract for dutchAuction
+/// @author Somyaditya Deopuria
+/// @notice You can use this contract for duction auction of fractional parts (ERC20) tokens of the ERC721 NFT
+/// @dev All function calls are currently implemented without side effects
+/// @custom:experimental This is an experimental contract.
+
+
 contract dutchAuction is ReentrancyGuard {
     uint256 auctionId;
     DragonFractionalNFT public fractionMachine;
+    
+    /* 
+    @dev Structure of Auction Item
+    @param tokenId: token id of the NFT
+    @param amount: Total amount of ERC20 tokens(Fractions of ERC721) to be sold
+    @param seller: wallet address of the seller
+    @param startingPrice: Price at which the auction will start
+    @param discountRate: Rate of the decrement of the price (rate % in hours)
+    @param expiresAt: Deadline of the auction
+    @param amountsold: Amount of ERC20 tokens that are sold
+     */
     struct AuctionItem{
         uint256 tokenId;
         uint256 amount;
@@ -33,9 +52,24 @@ contract dutchAuction is ReentrancyGuard {
         uint256 amount
     );
     mapping(uint256 => AuctionItem) public auctionInfo;
+
+    /* 
+    @dev Initializes the contract
+    @param splitter, the contract address of the fractional NFT making contract
+     */
     constructor(DragonFractionalNFT splitter){
         fractionMachine = splitter;
     }
+
+    /* 
+    @dev Creates Auction of the tokenId's amount of fractional part
+    @param tokenId: token id of the NFT
+    @param amount: Total amount of ERC20 tokens(Fractions of ERC721) to be sold
+    @param endTime: Deadline of the auction
+    @param startPrice: Price at which the auction will start
+    @param discount: Rate of the decrement of the price (rate % in hours)
+    @dev Updates the struct of AuctionItem
+     */
     function createAuction(uint256 tokenId, uint256 amount, uint256 endTime, uint256 startPrice, uint256 discount) external nonReentrant {
         address conadd = fractionMachine.checkFractionalContract(tokenId);
         require(conadd != address(0), "Fractions Not Present");
@@ -64,13 +98,19 @@ contract dutchAuction is ReentrancyGuard {
             );
 
     }
-
+    /* 
+        @dev calculates the price of each token according to the time elapsed
+        @return the current price of each token of a particular NFT
+    */
     function checkPrice(uint256 tokenId) public view returns(uint256) {
         uint256 timeElapsed = block.timestamp - auctionInfo[tokenId].startAt / (60*60);
         uint256 discount = auctionInfo[tokenId].discountRate * timeElapsed / (100*100);
         return (auctionInfo[tokenId].startingPrice - discount);
     }
-
+    /* 
+    @dev checks whether an auction is live or not
+    @return the boolean value whether the auction is live or not
+     */
    function isAuctionLive(uint256 tokenId) public view returns(bool){
         if(
             auctionInfo[tokenId].expiresAt > block.timestamp && 
@@ -83,6 +123,14 @@ contract dutchAuction is ReentrancyGuard {
                 return false;
             }
     }
+
+    /* 
+    @dev buys the amount of token 
+    @param tokenId: NFT token id of which parts are being bought
+    @param amount: amount of tokens (fractional parts that are being bought
+    @dev transfers the appropriate value of the amount of tokens from msg.sender to seller
+    @dev transfers the amount of tokens that are being sold to the buyer (msg.sender)
+     */
 
     function buyNow(uint256 tokenId, uint256 amount) external nonReentrant payable {
         require(isAuctionLive(tokenId)==true, "Already Sold or Ended");
